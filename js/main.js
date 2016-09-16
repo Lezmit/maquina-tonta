0b609ead1772dbdeef3f7bf1490b40972c45683c
lezmit <lezmit.gs@hotmail.com>
2016-09-16 09:51:59 -0500

    actualizando

--- js/main.js
+++ js/main.js
@@ -1,8 +1,45 @@
-window.addEventListener("load",function(){
-    var button = document.getElementById("calcularCri");
-    button.addEventListener("click",function(){
-         document.getElementById("bg-1").style.border="8px solid " + RandomColor({hue: 'green', count: 18});
-    });
-        
 
-});
+window.addEventListener("load", function() {
+    var supercri = document.getElementById("calcularSuperCri");
+    supercri.addEventListener("click", function() { 
+        var color = document.getElementById("Textcolor").value;
+        if(color == "red"){
+          document.getElementById("bg-1").classList.toggle("b-color1");
+          document.getElementById("bg-2").classList.remove("b-color2");
+          document.getElementById("bg-3").classList.remove("b-color3");
+        } 
+          else if(color == "yellow"){
+          document.getElementById("bg-2").classList.toggle("b-color2");
+          document.getElementById("bg-3").classList.remove("b-color3");
+          document.getElementById("bg-2").classList.remove("b-color1");
+        }
+          else if(color == "green"){
+          document.getElementById("bg-3").classList.toggle("b-color3");
+          document.getElementById("bg-1").classList.remove("b-color1");
+          document.getElementById("bg-2").classList.remove("b-color2");
+        }
+    });
+
+    var boton = document.getElementById("calcularCri");
+    var cont = 1;
+    boton.addEventListener("click", function(){
+        if (cont == 1){
+            document.getElementById("Textcolor").value = "red";
+            document.getElementById("bg-1").classList.add("b-color1");
+            document.getElementById("bg-3").classList.remove("b-color3");
+        }
+        if (cont == 2){
+            document.getElementById("Textcolor").value = "yellow";
+            document.getElementById("bg-2").classList.add("b-color2");
+            document.getElementById("bg-1").classList.remove("b-color1");
+        }
+        if (cont == 3){
+            document.getElementById("Textcolor").value = "green";
+            document.getElementById("bg-3").classList.add("b-color3");
+            document.getElementById("bg-2").classList.remove("b-color2");
+            cont = 0;
+        }
+        cont ++;
+    });
+
+ });
---
+++ readme.md
@@ -0,0 +1,4 @@
+#maquina-tonta
+### JS que permite cambiar de color de diferente manera con los botones cri y supercri  
+1.- del botont SUPER CRI  (si le das click varia el border de color de acuerdo al color ingresado). 
+2.- del boton CRI( color del border varia sin ingresar color y de forma consecutiva si seguimos dando click a CRI).
